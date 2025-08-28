a8d0bc588b0fbe577d5b99a3746a6ce0e9109bc1
Andrew-bot1 <aahayes27@amarillocollege.com>
2025-08-28 17:21:51 -0500

    Update Program.cs

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +8,20 @@ namespace Arrays
 {
     internal class Program
     {
-        
-        static void Main(string[] args)
-        { 
-
+        static void SortArray()
+        {
             int[] nums = new int[11] { 12, 7, 4, 27, 1, 13, 19, 6, 30, 9, 8 };
             int[] sortNums = new int[11];
-            int low = nums[0];
-            for (int i = 0; i<nums.Length;i++)
+
+            for (int i = 0; i < nums.Length; i++)
             {
+                int low = 999;
                 for (int j = 0; j < nums.Length; j++)
                 {
                     if (nums[j] < low)
                     {
                         low = nums[j];
+
                     }
                 }
 
@@ -34,16 +34,59 @@ namespace Arrays
                         nums[j] = 999999;
                     }
                 }
+            }
+            foreach (int i in sortNums)
+            {
+                Console.WriteLine(i);
+            }
+        }
 
+        static void SortMultiArry()
+        {
+            int[,] nums = { {54,3,21,50,57,7,73,24,85,79}, {33,11,19,33,51,72,80,6,78,25}, {1,65,4,86,71,52,16,77,29,13}, {23,44,90,70,28,17,53,64,62,83}, {55,98,22,89,34,75,56,58,27,35}, {87,88,8,61,74,14,66,20,59,81}, {18,76,2,5,26,84,15,82,9,60} };
+            int[,] sortNums = { { }, { }, { }, { }, { }, { } };
 
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                int low = 999;
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    for (int k = 0; k < nums.GetLength(1); k++)
+                    {
+                        if (nums[i, k] < low)
+                        {
+                            low = nums[j,k];
+                        }
+                    }
+                    if (nums[i, j] < low)
+                    {
+                        low = nums[i, j];
+                    }
+                    sortNums[i, j] = low;
+
+                    for (int k = 0; k < nums.GetLength(1); k++)
+                    {
+                        if (nums[j,k] == low)
+                        {
+                            nums[j,k] = 999999;
+                        }
+                    }
+                }
+
+                
             }
 
             foreach (int i in sortNums)
             {
                 Console.WriteLine(i);
-
-
             }
+
+        }
+        static void Main(string[] args)
+        { 
+            SortArray();
+            SortMultiArry();
+
         }
     }
 }
